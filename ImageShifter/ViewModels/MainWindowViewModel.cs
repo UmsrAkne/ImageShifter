6a5d4981dd64c864a33b8f90f8763a6b4553b6e3
UmsrAkne <sibata486@gmail.com>
2025-09-02 18:24:04 +0900

    init

---
+++ ImageShifter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ImageShifter.Utils;
+using Prism.Mvvm;
+
+namespace ImageShifter.ViewModels;
+
+public class MainWindowViewModel : BindableBase
+{
+    private AppVersionInfo appVersionInfo = new();
+
+    public string Title => appVersionInfo.GetAppNameWithVersion();
+}
